6607f46d78106f1bf3ff7225f87b304d2b429ad8
Vikas Irman Wansyah <143860032+VIKASIRMANWANSYAH@users.noreply.github.com>
2023-09-09 06:53:44 +0700

    Add files via upload

---
+++ soal ganjil.py
@@ -0,0 +1,17 @@
+print("\nmembuat perhitungan suhu\n")
+
+      #soal no 1 celcius menjadi fahrenheit
+#celcius = float(input("masukkan suhu : "))
+#print("suhu adalah",celcius, "celcius")
+#fahrenheit = ((9/5) * celcius) + 32
+#print("suhu adalah",fahrenheit,"fahrenheit")
+
+fahrenheit = float(input("masukkan suhu : "))
+print("suhu adalah", fahrenheit, "celcius")
+celcius = (5/9* (fahrenheit-32))
+print("suhu adalah",celcius,"celcius")
+
+
+
+
+                
